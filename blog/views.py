b20fb6c7b18479bbb33eb5b46a6f6e02bc904f7b
jgrugru <jeff.gruenbaum@gmail.com>
2021-08-13 14:27:34 -0500

    finished tutorial

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from blog.forms import CommentForm
 from django.shortcuts import render
 from blog.models import Post, Comment
+from .forms import CommentForm
 
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
@@ -22,10 +24,22 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": form,
     }
-
     return render(request, "blog_detail.html", context)
